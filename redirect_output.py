ef8746924c8df5a5fc5e5c5e3060f31eea3e4e9b
snehasish <rsnhasish125@gmail.com>
2025-05-11 13:02:57 +0530

    fixes++

--- redirect_output.py
+++ redirect_output.py
@@ -3,6 +3,13 @@ import os
 
 # nessecery to run the fastapi server | otherwise it will not work
 
-logfile = os.path.join(os.path.dirname(sys.executable), "app.log")
-sys.stdout = open(logfile, "a", buffering=1)
-sys.stderr = open(logfile, "a", buffering=1)
+# Determine a user-writable directory for the log file
+log_dir = os.getenv('LOCALAPPDATA', os.path.expanduser('~'))
+log_file = os.path.join(log_dir, 'KivstarTV', 'app.log')
+
+# Ensure the directory exists
+os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+# Redirect output to the log file
+sys.stdout = open(log_file, "a", buffering=1)
+sys.stderr = open(log_file, "a", buffering=1)
